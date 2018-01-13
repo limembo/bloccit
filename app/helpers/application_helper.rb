e550eead44dd87dcfbcf74be852db3969a81ed39
Liane Membis <lmembis@gmail.com>
2018-01-13 13:59:04 -0500

    Added gravatar feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
 module ApplicationHelper
-
-  def form_group_tag(errors, &block)
+  # #4 define a method named form_group_tag which takes two arguments. The first argument is an array of errors, and the second is a block. The & turns the block into a Proc which is a block that can be resued like a variable
+   def form_group_tag(errors, &block)
      css_class = 'form-group'
      css_class << ' has-error' if errors.any?
-     
+   # #5 the content_tag helper method is called. This method is used to build the HTML and CSS to display the form element and any associated errors. Helpers are written in Ruby and usually return HTML. It takes a symbol argument, a block, and an options hash. It then creates a symbol-specified HTML tag with the block contents, and if specified, the options
      content_tag :div, capture(&block), class: css_class
+   end
+
+   def avatar_url(user)
+   gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+   "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
    end
 end
