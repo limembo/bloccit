6a1d9a2b03ea762c6beef3e2b79d609e283a357f
Liane Membis <lmembis@gmail.com>
2018-01-13 08:16:38 -0500

    Added validation feature

---
+++ spec/controllers/sponsored_post_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+include RandomData
+
+RSpec.describe SponsoredPostsController, type: :controller do
+
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,14 +7,20 @@ RSpec.describe Post, type: :model do
  let(:title) { RandomData.random_sentence }
  let(:body) { RandomData.random_paragraph }
 
- # #3
+
    let(:topic) { Topic.create!(name: name, description: description) }
- # #4
+
    let(:post) { topic.posts.create!(title: title, body: body) }
 
-   it { is_expected.to belong_to(:topic) }
+   it { is_expected.to belong_to(:topic)}
 
- # #2
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
+
    describe "attributes" do
      it "has title and body attributes" do
       expect(post).to have_attributes(title: title, body: body)
