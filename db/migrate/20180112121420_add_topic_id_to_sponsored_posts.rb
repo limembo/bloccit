bb924a99c7a3f914e10b37f11dd3b9c1cd043905
Liane Membis <lmembis@gmail.com>
2018-01-22 07:11:47 -0500

    Removing sponsored post migrations

--- db/migrate/20180112121420_add_topic_id_to_sponsored_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddTopicIdToSponsoredPosts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :sponsored_posts, :topic_id, :integer
-    add_index :sponsored_posts, :topic_id
-  end
-end
